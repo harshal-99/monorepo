eeda3e37b4c6613af197a07b9d928e0b4c0b6216
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2023-10-29 11:56:16 +0530

    Implement Email filter component

--- packages/email-client/app/shared/email/components/EmailFilter.tsx
+++ packages/email-client/app/shared/email/components/EmailFilter.tsx
@@ -1,3 +1,53 @@
+import { Box, Button, Text } from "@mantine/core";
+import { useSetAtom } from "jotai";
+import { EmailFilterAtom, TEmailFilterAtom } from "../atoms";
+import { useTransition } from "react";
+
 export const EmailFilter = () => {
-  return <div>Email Filter</div>;
+  const setFilter = useSetAtom(EmailFilterAtom);
+  const [isPending, startTransition] = useTransition();
+  const handleFilterChange = (filter: TEmailFilterAtom) => {
+    startTransition(() => setFilter(filter));
+  };
+
+  return (
+    <Box
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Text>Filter by:</Text>
+      <Button.Group>
+        <Button
+          loading={isPending}
+          loaderProps={{ type: "dots" }}
+          onClick={() => handleFilterChange("all")}
+        >
+          All
+        </Button>
+        <Button
+          loading={isPending}
+          loaderProps={{ type: "dots" }}
+          onClick={() => handleFilterChange("read")}
+        >
+          Read
+        </Button>
+        <Button
+          loading={isPending}
+          loaderProps={{ type: "dots" }}
+          onClick={() => handleFilterChange("unread")}
+        >
+          Unread
+        </Button>
+        <Button
+          loading={isPending}
+          loaderProps={{ type: "dots" }}
+          onClick={() => handleFilterChange("favorites")}
+        >
+          Favriots
+        </Button>
+      </Button.Group>
+    </Box>
+  );
 };
